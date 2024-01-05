63f30c5c364d5056be9a86fd450d26678dcdc4f7
Antonio Rodriguez Chagoya <a01731037@itesm.mx>
2024-01-04 19:21:19 -0600

    Initial commit

---
+++ src/index.mjs
@@ -0,0 +1,71 @@
+import "./styles.css";
+
+const rive = require("@rive-app/canvas");
+
+// ----
+// LOCAL RIVE EXAMPLE
+// ----
+
+const layout = new rive.Layout({
+  fit: rive.Fit.Contain, // Change to: rive.Fit.Contain, or Cover
+  alignment: rive.Alignment.Center,
+});
+
+const riveCanvas = document.getElementById("rive-canvas");
+const riveButton = document.getElementById("rive-button");
+const riveStart = document.getElementById("start-button");
+
+const riveInstance = new rive.Rive({
+  // Load a local riv `clean_the_car.riv` or upload your own!
+  src: "erisyon-animation.riv",
+  // Be sure to specify the correct state machine (or animation) name
+  stateMachines: "State Machine 1", // Name of the State Machine to play
+  canvas: riveCanvas,
+  layout: layout, // This is optional. Provides additional layout control.
+  onLoad: () => {
+    // Prevent a blurry canvas by using the device pixel ratio
+    riveInstance.resizeDrawingSurfaceToCanvas();
+  },
+});
+
+// Resize the drawing surface if the window resizes
+window.addEventListener(
+  "resize",
+  () => {
+    riveInstance.resizeDrawingSurfaceToCanvas();
+  },
+  false
+);
+
+riveStart.addEventListener(
+  "click",
+  () => {
+    riveInstance.play();
+  },
+  false
+);
+riveButton.addEventListener(
+  "click",
+  () => {
+    riveInstance.reset();
+  },
+  false
+);
+
+// ---------------------------------
+
+// ----
+// HOSTED RIVE EXAMPLE
+// Comment out the code above and uncomment the code below.
+// ----
+
+// const riveHostedInstance = new rive.Rive({
+//   // Hosted .riv asset.
+//   src: "https://cdn.rive.app/animations/vehicles.riv",
+//   stateMachines: "bumpy",
+//   canvas: riveCanvas,
+//   autoplay: true,
+//   onLoad: () => {
+//     riveHostedInstance.resizeDrawingSurfaceToCanvas();
+//   }
+// });
